593f2d174dd6e8478deb1c6ec27dc05e977a33a2
Nay Zaw Aung Win <nay_win@hakkoda.io>
2023-01-31 01:11:34 +0000

    removed the confusing table aliases in fct_customer_orders.sql

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -24,46 +24,46 @@ customer_order_history as (
 
     select 
 
-        b.customer_id,
-        b.full_name,
-        b.surname,
-        b.givenname,
+        customers.customer_id,
+        customers.full_name,
+        customers.surname,
+        customers.givenname,
         min(order_date) as first_order_date,
         min(case 
-                when a.order_status not in ('returned','return_pending') 
+                when orders.order_status not in ('returned','return_pending') 
                 then order_date 
             end) as first_non_returned_order_date,
         max(case 
-                when a.order_status not in ('returned','return_pending') 
+                when orders.order_status not in ('returned','return_pending') 
                 then order_date 
             end) as most_recent_non_returned_order_date,
         coalesce(max(user_order_seq), 0) as order_count,
         coalesce(count(case 
-                            when a.order_status != 'returned' 
+                            when orders.order_status != 'returned' 
                             then 1 
                         end), 0) as non_returned_order_count,
         sum(case 
-                when a.order_status not in ('returned','return_pending') 
-                then c.payment_amount
+                when orders.order_status not in ('returned','return_pending') 
+                then payments.payment_amount
                 else 0 
             end) as total_lifetime_value,
         sum(case 
-                when a.order_status not in ('returned','return_pending') 
-                then c.payment_amount
+                when orders.order_status not in ('returned','return_pending') 
+                then payments.payment_amount
                 else 0 
             end) / nullif(count(case 
-                                when a.order_status not in ('returned','return_pending') 
+                                when orders.order_status not in ('returned','return_pending') 
                                 then 1 
                               end), 0) as avg_non_returned_order_value,
-        array_agg(distinct a.order_id) as order_ids
+        array_agg(distinct orders.order_id) as order_ids
 
-    from orders a
+    from orders
 
-    join customers b
-    on a.customer_id = b.customer_id
+    join customers
+    on orders.customer_id = customers.customer_id
 
-    left outer join payments c
-    on a.order_id = c.order_id
+    left outer join payments
+    on orders.order_id = payments.order_id
 
     group by 1, 2, 3, 4
 
